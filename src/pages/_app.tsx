4f068361b8eac2b2b45caba4bc93e6991eabb7fe
Szymon Stępień <st.szymon.tom@gmail.com>
2022-07-25 06:49:21 +0200

    feat(utils): add  util  and  enum

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,25 +1,26 @@
-// src/pages/_app.tsx
-import { withTRPC } from "@trpc/next";
-import type { AppRouter } from "../server/router";
-import type { AppType } from "next/dist/shared/lib/utils";
-import superjson from "superjson";
-import { SessionProvider } from "next-auth/react";
-import "../styles/globals.css";
+import { withTRPC } from '@trpc/next';
+import superjson from 'superjson';
+import { SessionProvider } from 'next-auth/react';
+import type { AppType } from 'next/dist/shared/lib/utils';
+
+import { isWindow } from 'utils/isWindow';
+
+import type { AppRouter } from '../server/router';
+
+import '../styles/globals.css';
 
 const MyApp: AppType = ({
   Component,
   pageProps: { session, ...pageProps },
-}) => {
-  return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
-  );
-};
+}) => (
+  <SessionProvider session={session}>
+    <Component {...pageProps} />
+  </SessionProvider>
+);
 
 const getBaseUrl = () => {
-  if (typeof window !== "undefined") {
-    return "";
+  if (isWindow) {
+    return '';
   }
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
 
@@ -28,23 +29,12 @@ const getBaseUrl = () => {
 
 export default withTRPC<AppRouter>({
   config({ ctx }) {
-    /**
-     * If you want to use SSR, you need to use the server's full URL
-     * @link https://trpc.io/docs/ssr
-     */
     const url = `${getBaseUrl()}/api/trpc`;
 
     return {
-      url,
       transformer: superjson,
-      /**
-       * @link https://react-query.tanstack.com/reference/QueryClient
-       */
-      // queryClientConfig: { defaultOptions: { queries: { staleTime: 60 } } },
+      url,
     };
   },
-  /**
-   * @link https://trpc.io/docs/ssr
-   */
   ssr: false,
 })(MyApp);
