5507eacb7374a559aaa424ebb16b9ce32acdfea0
Szymon Stępień <st.szymon.tom@gmail.com>
2022-07-26 07:55:27 +0200

    feat(components): add loader component

---
+++ src/components/loader/loader.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import Image from 'next/image';
+
+import loaderIconPath from '@static/assets/loader.svg';
+
+export const Loader = () => <Image src={loaderIconPath} />;
