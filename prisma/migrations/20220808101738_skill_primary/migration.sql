c52719dd1c5531b05acf31b310d129d99b9e2dbd
Szymon Stępień <st.szymon.tom@gmail.com>
2023-02-07 07:17:33 +0100

    Integrate with planetscale

--- prisma/migrations/20220808101738_skill_primary/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Skill" ADD COLUMN     "primary" BOOLEAN;
--- prisma/migrations/20220823202318_user_socials/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "cvUrl" TEXT,
-ADD COLUMN     "linkedinUrl" TEXT;
